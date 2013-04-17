a01d7dfa17eb549160233827533393da6c78aa5b
beckdaniel <daniel.e.beck@gmail.com>
2013-04-17 19:53:45 +0100

    tests on oracle

--- skal/skal.py
+++ skal/skal.py
@@ -1,4 +1,8 @@
 
+import numpy as np
+from sklearn import clone
+from sklearn.metrics import mean_absolute_error as MAE
+import pdb
 
 class ActiveLearner(object):
     """An active learner.
@@ -6,14 +10,29 @@ class ActiveLearner(object):
     
     def __init__(self, estimator, extra=None):
         """init
-        extra: densities, X_test, y_test
+        extra: densities, X_test, y_test, bagsize, numbags, y_query, X
         """
-        self.estimator = estimator
+        self.estimator = clone(estimator)
         self.extra = extra
 
+    def _get_oracle_mae(self, i, instance):
+        estimator = clone(self.estimator)
+        new_X = np.concatenate((self.extra['X'], [instance]), axis=0)
+        new_y = np.concatenate((self.extra['y'], [self.extra['y_query'][i]]))
+        estimator.fit(new_X, new_y)
+        return MAE(estimator.predict(self.extra['X_test']), self.extra['y_test'])
+        
+
     def _query(self, X, strategy):
-        if strategy = "random":
-            pass
+        if strategy == "oracle":
+            best_mae = 1000
+            for i, instance in enumerate(X):
+                mae = self._get_oracle_mae(i, instance)
+                if mae < best_mae:
+                    best_mae = mae
+                    best_instance = instance
+                    best_i = i
+        return (best_i, best_instance)
 
     def query(self, X, strategy):
         """query
---
+++ test/test_skal.py
@@ -0,0 +1,68 @@
+
+import numpy as np
+from ..skal.skal import ActiveLearner
+
+class MockMeanEstimator(object):
+
+    def __init__(self):
+        self.params = {}
+
+    def fit(self, X, y):
+        self.mean = np.mean(y)
+
+    def predict(self, X_test):
+        return np.array([self.mean] * X_test.shape[0])
+
+    def get_params(self, deep=False):
+        return self.params
+
+
+def test_oracle_strategy_1():
+    X = np.random.randint(100, size=(10,5))
+    y = np.array([7] * 10)
+    X_query = np.random.randint(100, size=(10,5))
+    est = MockMeanEstimator()
+    extra = {}
+    extra['y_query'] = np.array(range(10))
+    extra['X_test'] = np.random.randint(100, size=(5,5))
+    extra['y_test'] = np.array([7] * 5)
+    extra['X'] = X
+    extra['y'] = y
+    al = ActiveLearner(est, extra)
+    al.estimator.fit(X, y)
+    q = al.query(X_query, "oracle")
+    assert (q == X_query[7]).all()
+
+
+def test_oracle_strategy_2():
+    X = np.random.randint(100, size=(10,5))
+    y = np.array([7] * 10)
+    X_query = np.random.randint(100, size=(10,5))
+    est = MockMeanEstimator()
+    extra = {}
+    extra['y_query'] = np.array(range(10))
+    extra['X_test'] = np.random.randint(100, size=(5,5))
+    extra['y_test'] = np.array([6] * 5)
+    extra['X'] = X
+    extra['y'] = y
+    al = ActiveLearner(est, extra)
+    al.estimator.fit(X, y)
+    q = al.query(X_query, "oracle")
+    assert (q == X_query[0]).all()
+
+
+def test_oracle_strategy_3():
+    X = np.random.randint(100, size=(10,5))
+    y = np.array([7] * 10)
+    X_query = np.random.randint(100, size=(10,5))
+    est = MockMeanEstimator()
+    extra = {}
+    extra['y_query'] = np.array(range(10))
+    extra['X_test'] = np.random.randint(100, size=(5,5))
+    extra['y_test'] = np.array([8] * 5)
+    extra['X'] = X
+    extra['y'] = y
+    al = ActiveLearner(est, extra)
+    al.estimator.fit(X, y)
+    q = al.query(X_query, "oracle")
+    assert (q == X_query[9]).all()
